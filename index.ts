b2c92990504e54fd3762a3277f9df7b655829d18
Silaaydin23 <silaaydn88@gmail.com>
2026-02-05 10:00:06 +0300

    sunucu ve veritabani baglantisi yapildi

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import { Elysia } from 'elysia';
+import postgres from 'postgres';
+
+const sql = postgres({
+    host: 'localhost',      
+    port: 5432,             
+    database: 'postgres',   
+    username: 'postgres',   
+    password: process.env.DB_PASSWORD,     
+});
+
+const app= new Elysia();
+
+
+app.get('/', async () => {
+    
+    const sonuc = await sql`SELECT NOW()`;
+    console.log("Veritabani zamani:", sonuc);
+    
+    return "Sunucu calisiyor. Veritabani baglantisi tamam.";
+});
+
+app.listen(3000);
+
+console.log("Sunucu basladi: http://localhost:3000");
